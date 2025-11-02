a4a144fe1d2a622cc7f752c073f2ffc725cde605
mily <mildred_arr07@hotmail.com>
2025-11-02 15:15:14 -0600

    Se agrego la funcion showRecipe, para el funcionamiento del API

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,5 +9,24 @@ const timeout = function (s) {
 };
 
 
-// https://forkify-api.herokuapp.com/v2
 
+///////////////////////////////////////
+async function showRecipe() {
+  try {
+    // URL Correcta
+    const resp = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    // URL INcorrecta.
+  //  const resp = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzz');
+    const data = await resp.json();// aqui convierte  la respuesta a jason, para ello se delcara una constante data y no olvidar usar el await
+    console.log('Respuesta:', resp); // enviar la consol la constante resp.
+    console.log('Data:', data);  // enviar a la consola la constante data.
+
+    const recipe = data.data.recipe; // Para visualizar los datos que se necesitan desplegar en la pantalla. 
+    console.log('Receta:', recipe);
+
+  } catch (err) {
+    alert('Error: ' + err);// en caso de error, enviar mensaje en alert
+  }
+}
+
+showRecipe();// Invocar la funcion showRecipe.
